0fa9e37241928efbd3d272e97c64dc79df4160ff
klreeher <k.reeher@protonmail.com>
2020-10-23 11:23:16 -0500

    == not is

---
+++ endpoint/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+
+#logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+base_url = 'https://test-api.service.hmrc.gov.uk'
+
+headers = {
+	'Accept' : 'application/vnd.hmrc.1.0+json'
+}
--- tests/empty_test.py
+++ tests/empty_test.py
@@ -1,3 +1,26 @@
+import requests
+import endpoint
+from endpoint import base_url, headers
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+def test_hello():
+
+    r = requests.get(endpoint.base_url+'/hello')
+    logger.info(json.dumps(r.json()))
+
+    assert r.status_code == requests.codes['not_found']
+
+def test_helloworld():
+
+    r = requests.get(endpoint.base_url+'/hello/world', headers=endpoint.headers)
+
+    logger.info(json.dumps(r.json()))
+
+    assert r.status_code is requests.codes.ok
+
 def test_empty():
     """
     PyTest tests are callables whose names start with "test"
